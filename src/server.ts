93e96bead01212e0dff26c23b4a23fc7adbd74a2
AlexSodreDev <alexandre.cortines@gmail.com>
2021-04-30 22:14:50 -0300

    route test message

--- src/server.ts
+++ src/server.ts
@@ -6,4 +6,8 @@ app.get("/cep", (request, response) => {
   return response.json({ message: "Hello World - SOAP" })
 });
 
+app.post("/cep", (request, response) => {
+  return response.json({ message: "Dados salvos com sucesso!" })
+});
+
 app.listen(3333, () => console.log("✔✔ Server running 🚀 ✔✔"));
