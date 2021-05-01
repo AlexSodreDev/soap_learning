8a861220ea07de5db89e23425ee39fb54abf5d06
AlexSodreDev <alexandre.cortines@gmail.com>
2021-04-30 21:20:08 -0300

    route test message

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,8 @@ import express from 'express';
 
 const app = express();
 
+app.get("/cep", (request, response) => {
+  return response.json({ message: "Hello World - SOAP" })
+});
+
 app.listen(3333, () => console.log("✔✔ Server running 🚀 ✔✔"));
