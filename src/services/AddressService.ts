29c773c55c5aabe16e46a608545607a1f9c42a60
AlexSodreDev <alexandre.cortines@gmail.com>
2021-05-12 19:25:22 -0300

    delete findAddressesByCepWithCallback

--- src/services/AddressService.ts
+++ src/services/AddressService.ts
@@ -6,21 +6,6 @@ class AddressService {
 
   private readonly WSDL_CORREIOS = process.env.WSDL_CORREIOS;
 
-  public findAddressByCepWithCallback(cep: any, cb) {
-    soap.createClient(this.WSDL_CORREIOS, (err, client) => {
-      if (err) {
-        console.log(err)
-      }
-      client.consultaCEP({ cep: cep }, (err, result) => {
-        if (err) {
-          console.log(err)
-          cb(err, null)
-        }
-        cb(null, result)
-      });
-    });
-  }
-
   public async findAddressByCepTransformToPromise(cep) {
     return new Promise((resolve, reject) => {
       soap.createClient(this.WSDL_CORREIOS, (err, client) => {
